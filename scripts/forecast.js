47264ef8bde1cdfdcf9ce580e4a4e55e937c492a
Khandakar Zahidur Rahim Hridoy <kzrhridoy@gmail.com>
2022-03-19 18:57:45 +0600

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,16 @@
 const key = '2AtmDz8W7BNLxsZVYVxDbdBw65GNcC5G';
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity('dhaka')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
